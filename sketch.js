c5e5e8d52647e13b53f6fa92b5e1773c188c039e
Hu Youyang <61373926+huyyoo@users.noreply.github.com>
2021-06-17 23:07:34 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,36 @@
-let video;
-
-function preload(){
-  video = createVideo('background.mp4');
-  video.loop();
-}
+let myVideo;
+let otherVideo;
 
 function setup() {
-  createCanvas(innerWidth, innerHeight);
+  createCanvas(1000, 1000);
   
+    myVideo = createCapture(VIDEO, 
+      function(stream) {
+        let p5l = new p5LiveMedia(this, "CAPTURE", stream, "jZQ64AMJc_TESTTEST");
+        p5l.on('stream', gotStream);
+      }
+    );  
+    myVideo.muted = true;     
+    myVideo.hide();
 }
 
 function draw() {
-  image(video,width,height);
+  background(240);
+  stroke(255);
+  if (myVideo != null) {
+    image(myVideo,0,0,320,240);
+    text("My Video", 10, 10);
+  }
+  if (otherVideo != null) {
+    image(otherVideo,320,0,320,240);
+    text("Their Video", 640+10, 10);
+  }  
+}
+
+// We got a new stream!
+function gotStream(stream, id) {
+  // This is just like a video/stream from createCapture(VIDEO)
+  otherVideo = stream;
+  //otherVideo.id and id are the same and unique identifiers
+  otherVideo.hide();
 }
