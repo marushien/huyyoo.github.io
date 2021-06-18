b7456dc10d6bc9f4580dc734b8deceab1b9a093f
Hu Youyang <61373926+huyyoo@users.noreply.github.com>
2021-06-18 10:49:10 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,36 +1,23 @@
-let myVideo;
 let otherVideo;
 
+
 function setup() {
-  createCanvas(1000, 1000);
-  
-    myVideo = createCapture(VIDEO, 
-      function(stream) {
-        let p5l = new p5LiveMedia(this, "CAPTURE", stream, "jZQ64AMJc_TESTTEST");
-        p5l.on('stream', gotStream);
-      }
-    );  
-    myVideo.muted = true;     
-    myVideo.hide();
+  myCanvas = createCanvas(1000, 1000);
+  let p5l = new p5LiveMedia(this,"CANVAS",myCanvas,"party");
+  p5l.on('stream', gotStream);   
 }
 
 function draw() {
-  background(240);
-  stroke(255);
-  if (myVideo != null) {
-    image(myVideo,0,0,320,240);
-    text("My Video", 10, 10);
+  background(220);
+  if(otherVideo != null){
+    image(otherVideo,0,0,width,height);
   }
-  if (otherVideo != null) {
-    image(otherVideo,320,0,320,240);
-    text("Their Video", 640+10, 10);
-  }  
 }
 
 // We got a new stream!
-function gotStream(stream, id) {
+function gotStream(stream) {
   // This is just like a video/stream from createCapture(VIDEO)
   otherVideo = stream;
-  //otherVideo.id and id are the same and unique identifiers
+  //otherVideo.id is the unique identifier for this peer
   otherVideo.hide();
 }
